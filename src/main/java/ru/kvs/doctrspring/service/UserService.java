90e3d23d15fae5b672bde74e498348dcc230a9d4
Vitaly Kolesnikov <vkolesnikov@custis.ru>
2022-08-27 17:56:56 +0300

    add Dockerfile

--- src/main/java/ru/kvs/doctrspring/service/UserService.java
+++ src/main/java/ru/kvs/doctrspring/service/UserService.java
@@ -2,7 +2,6 @@ package ru.kvs.doctrspring.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
