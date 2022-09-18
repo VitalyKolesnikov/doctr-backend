e0c049a8131702e6595c5481e73f8b29b3cee212
Vitaly Kolesnikov <vkolesnikov@custis.ru>
2022-09-18 17:02:42 +0300

    feat: Update security

--- src/main/java/ru/kvs/doctrspring/config/SecurityConfig.java
+++ src/main/java/ru/kvs/doctrspring/config/SecurityConfig.java
@@ -56,7 +56,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(LOGIN_ENDPOINT).permitAll()
                 .antMatchers(API_DOCS, SWAGGER_UI, SWAGGER_UI_INDEX).permitAll()
                 .antMatchers(ADMIN_ENDPOINT).hasRole("ADMIN")
-                .anyRequest().authenticated()
                 .and()
                 .apply(new JwtConfigurer(jwtTokenProvider));
     }
